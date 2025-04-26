fda150f53efc6568ecda2a686a7062c73d62747e
applandsys <applandsys@gmail.com>
2025-04-26 17:10:27 +0600

    check ifno

--- index.js
+++ index.js
@@ -151,27 +151,8 @@ verifier.verify( 'amarsonarbangla255555255585@yahoo.com', function( err, info ){
   if( err ) console.log(err);
   else{
     console.log( "Success (T/F): " + info.success );
-    console.log( "Info: " + info.info );
+    console.log( "Info: " + info );
  
-    //Info object returns a code which representing a state of validation:
- 
-    //Connected to SMTP server and finished email verification
-    console.log(info.code === infoCodes.finishedVerification);
- 
-    //Domain not found
-    console.log(info.code === infoCodes.domainNotFound);
- 
-    //Email is not valid
-    console.log(info.code === infoCodes.invalidEmailStructure);
- 
-    //No MX record in domain name
-    console.log(info.code === infoCodes.noMxRecords);
- 
-    //SMTP connection timeout
-    console.log(info.code === infoCodes.SMTPConnectionTimeout);
- 
-    //SMTP connection error
-    console.log(info.code === infoCodes.SMTPConnectionError)
   }
 });
 
