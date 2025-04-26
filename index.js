0fd12d219382577f8ded6fda0427f59486c18f1b
applandsys <applandsys@gmail.com>
2025-04-26 17:08:13 +0600

    yahoo check

--- index.js
+++ index.js
@@ -144,28 +144,34 @@ emailExistence.check('samir_noyan2025dfsdfsdf@yahoo.com', function(error, respon
 
 
 // 2nd time 
-const validator = require('smtp-validate-email');
-
-const email = 'samir_noyan2025dfsdfsdf@yahoo.com';
-
-const options = {
-  sender: 'info@chatpix.xyz', // sender email address, must be real
-  fqdn: 'chatpix.xyz',        // your domain
-  timeout: 5000,                 // timeout in ms
-  port: 25,                      // port to connect
-};
-
-validator(email, options, function(err, result) {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    console.log('Result:', result);
-    if (result.success) {
-      console.log('Email address is valid!');
-    } else {
-      console.log('Invalid email address.');
-    }
+var verifier = require('email-verify');
+var infoCodes = verifier.infoCodes;
+ 
+verifier.verify( 'amarsonarbangla255555255585@yahoo.com', function( err, info ){
+  if( err ) console.log(err);
+  else{
+    console.log( "Success (T/F): " + info.success );
+    console.log( "Info: " + info.info );
+ 
+    //Info object returns a code which representing a state of validation:
+ 
+    //Connected to SMTP server and finished email verification
+    console.log(info.code === infoCodes.finishedVerification);
+ 
+    //Domain not found
+    console.log(info.code === infoCodes.domainNotFound);
+ 
+    //Email is not valid
+    console.log(info.code === infoCodes.invalidEmailStructure);
+ 
+    //No MX record in domain name
+    console.log(info.code === infoCodes.noMxRecords);
+ 
+    //SMTP connection timeout
+    console.log(info.code === infoCodes.SMTPConnectionTimeout);
+ 
+    //SMTP connection error
+    console.log(info.code === infoCodes.SMTPConnectionError)
   }
 });
-
 
