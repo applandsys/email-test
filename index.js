e6105f733f47080161a7e35afdda250f855718b6
applandsys <applandsys@gmail.com>
2025-04-26 15:58:12 +0600

    Email valid check

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("My Server is Running well");
+
+const dns = require('dns');
+
+const validateDomain = (email) => {
+  const domain = email.split('@')[1];
+  
+  dns.resolve(domain, 'MX', (err, addresses) => {
+    if (err) {
+      console.log('Invalid domain or no mail server found.');
+    } else {
+      console.log('Valid domain with mail server found:', addresses);
+    }
+  });
+};
+
+validateDomain('applandsys@gmail.com');
