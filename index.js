7363a1d5ddf7aaa66923e3d53d60bb52c2c7e8c0
applandsys <applandsys@gmail.com>
2025-04-26 17:02:40 +0600

    incaldi yahoo check

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ verifyEmail('applandsys@gmail.com')
 
   const emailExistence = require('email-existence');
 
-emailExistence.check('samir_noyan@yahoo.com', function(error, response){
+emailExistence.check('samir_noyan2025dfsdfsdf@yahoo.com', function(error, response){
   if (error) {
     console.error('Error:', error);
   } else {
